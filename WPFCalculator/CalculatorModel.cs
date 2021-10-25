45452e482b61328055fea8ff6975d4ae0e164f8b
Julien Creach <julien.creach@hotmail.fr>
2021-10-25 12:56:36 +0200

    Simple C# WPF Calculator

---
+++ WPFCalculator/CalculatorModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFCalculator
+{
+    public class CalculatorModel : INotifyPropertyChanged
+    {
+        private string displayContentValue = string.Empty;
+
+        public string DisplayContent
+        {
+            get => displayContentValue;
+            set
+            {
+                if (value != displayContentValue)
+                {
+                    displayContentValue = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
