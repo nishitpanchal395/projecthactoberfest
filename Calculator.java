f5ae1be289e97b8d080663ad3dc82fea678cb08b
Imran Khan <imran.techwiz@gmail.com>
2021-10-29 20:19:37 +0530

    JS Mouse Hover added

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
-//Calculator in java for Calculations
-
 public class calculator {
     public static void main(String[] args) {
 
         char operator;
         Double number1, number2, result;
         Scanner input = new Scanner(System.in);
-        System.out.println("Choose an operator: +, -, *, / or ^ ");
+        System.out.println("Choose an operator: +, -, *, or /");
         operator = input.next().charAt(0);
         System.out.println("Enter first number");
         number1 = input.nextDouble();
@@ -21,7 +19,6 @@ public class calculator {
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
                 break;
-                
             case '-':
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
@@ -31,17 +28,11 @@ public class calculator {
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
-                
             case '/':
                 result = number1 / number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
                 break;
-                
-            case '^':
-                result = Math.pow(number1, number2);
-                System.out.println(number1 + " ^ " + number2 + " = " + result);
-                break;
-                
+
             default:
                 System.out.println("Invalid operator!");
                 break;
