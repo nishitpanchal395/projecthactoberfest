e3a26300a612f2466c931cf1ea31230c2d1dbd5c
Shubham Biswas <shubhambiswas024@gmail.com>
2022-10-09 23:08:59 +0530

    Hactoberfest added new file codechef problem

---
+++ MintargetReach.java
@@ -0,0 +1,10 @@
+import java .util.*;
+class rupam {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in );
+        int a=1;
+        for (int i=1 ; i< 11;i ++){
+            System.out.println(i*a);
+        }
+    }
+}
