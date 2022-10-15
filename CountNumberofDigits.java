73628dc3737afa3d82332fe434767725feaa7d7b
prabhattt17 <rohit8282gupta@gmail.com>
2022-10-15 16:21:39 +0530

    modified CountNumberofDigits

--- CountNumberofDigits.java
+++ CountNumberofDigits.java
@@ -1,11 +1,12 @@
 class Solution {
     public int countNumberofDigits(int n) {
+        int ans=0;
         if (n >= 0) {
-            return (int) Math.log10(n) + 1;
+            ans= (int) Math.log10(n) + 1;
         } else {
             // To handle the case of negative number
-            return (int) Math.log10(Math.abs(n)) + 1;
+            ans= Math.log10(Math.abs(n)) + 1;
         }
-
+        return ans;
     }
 }
